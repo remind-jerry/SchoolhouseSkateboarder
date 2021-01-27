e212445d2f5252b3b87dd796244ee9f7772fb202
Oleg991 <o.n.eremenko@gmail.com>
2021-01-27 20:38:33 +0400

    Added fullscreen support

--- SchoolhouseSkateboarder/GameViewController.swift
+++ SchoolhouseSkateboarder/GameViewController.swift
@@ -15,18 +15,15 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                scene.size = view.bounds.size
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
             view.ignoresSiblingOrder = true
             view.showsFPS = true
             view.showsNodeCount = true
+
+            let scene = GameScene()
+
+            scene.scaleMode = .resizeFill
+
+            view.presentScene(scene)
         }
     }
 
